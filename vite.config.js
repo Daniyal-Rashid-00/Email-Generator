2a75ccb42466a07599e3c06f18bd4c2713db9794
Daniyal-Rashid-00 <everything4u1212@gmail.com>
2025-12-15 14:52:43 +0500

    Update

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // IMPORTANT: Change this to match your EXACT repository name!
-  // If your repo URL is: https://github.com/daniyal-rashid-00/email-writer
-  // Then use: base: '/email-writer/'
-  // 
+
   // Current setting assumes repo is named "Email-Generator"
   base: '/Email-Generator/',
   
